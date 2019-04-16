4bcff8b1d4f9bcd948c9135070a4cda6b68c2cde
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-16 14:30:42 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,12 +4,6 @@ class String
 
   def sentence?
    self.end_with?(".")
-     #"Hi, my name is Sophie".end_with?(".")
-    # "What's your name?".end_with?("?")
-    # "Happy Halloween!".end_with?("?")
-    # "Hi, my name is Sophie!".end_with?("?")
-    # "Hi, my name is Sophie.".end_with?("?")
-    
   end
 
   def question?
